9c215d56620a537c61548afd5e5f9e47876ab7b7
baemjoo <audwnzhfldk@naver.com>
2018-12-24 17:27:04 +0900

    .

--- src/servlet/TodayServlet.java
+++ src/servlet/TodayServlet.java
@@ -1,9 +1,10 @@
+package servlet;
 
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,28 +37,39 @@ public class TodayServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head><title>presenttime</title></head>");
-		out.println("<style>");
 		out.println("<body>");
-		out.println("<a href=\"./WebContent/index.html\">메인화면</a>");
+		out.println("<a href=\""+getServletContext().getContextPath()+"/index.html\">메인화면</a>");
 		
 		
 		//현재 시간
-		LocalDateTime now = LocalDateTime.now();
-		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분");
-		String pretime = now.format(dateTimeFormatter);
+//		LocalDateTime now = LocalDateTime.now();
+//		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년M월d일ah시m분");
+//		String pretime = now.format(dateTimeFormatter);
+//		
+//		int year = Integer.parseInt(pretime.substring(0, 4));
+//		int month = Integer.parseInt(pretime.substring(5,6));
+//		int mon_of_day = Integer.parseInt(pretime.substring(7,8));
+//		String pa = pretime.substring(9,10);
+//		
+//		int hour = Integer.parseInt(pretime.substring(10,11));
+//		int minute = Integer.parseInt(pretime.substring(13,14));
+//		
+//		out.println("현재 시간 : "+ year + month + pa + hour + minute);
 		
-		int year = Integer.parseInt(pretime.substring(0, 4));
-		int month = Integer.parseInt(pretime.substring(6,7));
-//		int day = Integer.parseInt(pretime.substring(9,10));
-		String pa = pretime.substring(12, 13);
+		LocalDate currentDate = LocalDate.now();
+		int year = currentDate.getYear();
+		int month = currentDate.getMonthValue();
+		int day = currentDate.getDayOfMonth();
 		
-		int hour = Integer.parseInt(pretime.substring(14,15));
-		int minute = Integer.parseInt(pretime.substring(17,18));
+		LocalTime currentTime = LocalTime.now();
+		int hour = currentTime.getHour();
+		int minute = currentTime.getMinute();
 		
-		out.println("현재 시간 : "+ year + month + pa + hour + minute);
+		out.println("현재시간 : " +year+"/"+month+"/"+day+" "+hour+":"+minute);
 		
 		out.println("</body>");
 		out.println("</html>");
+		out.close();
 	}
 
 	/**

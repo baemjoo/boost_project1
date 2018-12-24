73dd303ea1ce6a4d5b15b9f28ee24fb97593a695
baemjoo <audwnzhfldk@naver.com>
2018-12-24 14:35:22 +0900

    몇시예요 url 변경 및 servlet timeclass 적용

--- src/TodayServlet.java
+++ src/TodayServlet.java
@@ -2,6 +2,8 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,8 +36,26 @@ public class TodayServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head><title>presenttime</title></head>");
+		out.println("<style>");
 		out.println("<body>");
 		out.println("<a href=\"./WebContent/index.html\">메인화면</a>");
+		
+		
+		//현재 시간
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분");
+		String pretime = now.format(dateTimeFormatter);
+		
+		int year = Integer.parseInt(pretime.substring(0, 4));
+		int month = Integer.parseInt(pretime.substring(6,7));
+//		int day = Integer.parseInt(pretime.substring(9,10));
+		String pa = pretime.substring(12, 13);
+		
+		int hour = Integer.parseInt(pretime.substring(14,15));
+		int minute = Integer.parseInt(pretime.substring(17,18));
+		
+		out.println("현재 시간 : "+ year + month + pa + hour + minute);
+		
 		out.println("</body>");
 		out.println("</html>");
 	}
